cb78f0a776fd8cf8a0ba06e48749225f34e6e3e9
mika36 <46766024+mika36@users.noreply.github.com>
2019-02-20 18:57:16 -0400

    UrlInTextFinal

--- MikaelaAntezana/URLInText.py
+++ MikaelaAntezana/URLInText.py
@@ -1,13 +1,15 @@
 def urlInText(text):
-    size = len(text)
-    initialPosition = text.find("http://")
-    print(initialPosition)
+    lastPosition = ' '
+    counter = 1
+    initialPosition = text.find("http://") + 4
     currentPosition = initialPosition
-    while(currentPosition )
-    endPosition
+    previousText= 'http'
+    while(text[currentPosition] != lastPosition):
+        urlText = previousText+text[currentPosition]
+        previousText =urlText
+        currentPosition += 1
+    #else:
+    # print("no existe una URL en la cadena")
+    print(urlText)
 
-
-   #else:
-    #    print("no existe una URL en la cadena")
-
-urlInText("12http://")
+urlInText("12http://hello blabla")
